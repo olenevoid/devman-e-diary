243912fce19aea8418fab7501f2c6c2d24092381
olenevoid <mazekunov@gmail.com>
2025-08-08 11:28:00 +0500

    Change filtering to be case insensitive and work with cyrillic

--- scripts.py
+++ scripts.py
@@ -6,7 +6,7 @@ BAD_POINTS = (2, 3)
 
 
 def get_schoolkid(full_name: str) -> Schoolkid | None:
-    schoolkids = Schoolkid.objects.filter(full_name__contains=full_name).all()
+    schoolkids = Schoolkid.objects.filter(full_name__iregex=full_name).all()
     if len(schoolkids) > 1:
         print(f'Найдено несколько школьников с именем {full_name}')
         return None
