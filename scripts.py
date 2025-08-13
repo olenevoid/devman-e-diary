2bac4cc257c288f50791d69b80c616ab458f8b25
olenevoid <mazekunov@gmail.com>
2025-08-13 09:45:08 +0500

    Fix commendation typo

--- scripts.py
+++ scripts.py
@@ -9,7 +9,7 @@ from datacenter.models import (
 
 BAD_POINTS = (2, 3)
 GOOD_POINTS = 5
-DEFAULT_COMMEDATION_TEXT = 'Хорошо!'
+DEFAULT_COMMENDATION_TEXT = 'Хорошо!'
 
 
 def get_schoolkid(full_name: str) -> Schoolkid | None:
@@ -50,43 +50,43 @@ def delete_chastisements(name: str):
     print(f'Замечания успешно удалены для {schoolkid.full_name}')
 
 
-def get_last_lesson_without_commedation(
+def get_last_lesson_without_commendation(
         subject_name: str, 
         schoolkid: Schoolkid, 
     ) -> Lesson:
 
-    commedations = Commendation.objects.filter(
+    commendations = Commendation.objects.filter(
         schoolkid=schoolkid,
         subject__title__iregex=subject_name
     ).all()
 
-    commedation_dates = [commedation.created for commedation in commedations]
+    commendation_dates = [commendation.created for commendation in commendations]
 
     last_lesson = Lesson.objects.filter(
         year_of_study=schoolkid.year_of_study,
         group_letter=schoolkid.group_letter,
         subject__title__iregex=subject_name
-    ).exclude(date__in=commedation_dates).order_by('date').last()
+    ).exclude(date__in=commendation_dates).order_by('date').last()
 
     return last_lesson
 
 
-def create_commedation(
+def create_commendation(
         name: str,
         subject_name: str,
-        text: str = DEFAULT_COMMEDATION_TEXT
+        text: str = DEFAULT_COMMENDATION_TEXT
     ):
     schoolkid = get_schoolkid(name)
     if not schoolkid:
         return
 
-    lesson = get_last_lesson_without_commedation(subject_name, schoolkid)
+    lesson = get_last_lesson_without_commendation(subject_name, schoolkid)
 
-    commedation = Commendation()
-    commedation.created = lesson.date
-    commedation.subject = lesson.subject
-    commedation.schoolkid = schoolkid
-    commedation.teacher = lesson.teacher
-    commedation.text = text
-    commedation.save()
+    commendation = Commendation()
+    commendation.created = lesson.date
+    commendation.subject = lesson.subject
+    commendation.schoolkid = schoolkid
+    commendation.teacher = lesson.teacher
+    commendation.text = text
+    commendation.save()
     print(f'Похвала успешно создана для {schoolkid.full_name}')
